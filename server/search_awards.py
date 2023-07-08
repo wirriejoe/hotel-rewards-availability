81e8e7ee903212f81c70cdb25ce70b9af495dfc8
Willie Zhou <williezhou@gmail.com>
2023-07-08 14:47:22 -0400

    fix to load_dotenv()

--- server/search_awards.py
+++ server/search_awards.py
@@ -12,7 +12,7 @@ import logging
 
 # Load environment variables
 # load_dotenv(os.path.realpath(os.path.join(os.path.dirname(__file__), '../.env')))
-load_dotenv(os.path(dotenv_path='/etc/secrets/.env'))
+load_dotenv(dotenv_path='/etc/secrets/.env')
 
 # Set up logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
