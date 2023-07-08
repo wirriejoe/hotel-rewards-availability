fd1514d6a50c3eadb1f86746e3fa803bcf35311f
Willie Zhou <williezhou@gmail.com>
2023-07-08 19:48:24 -0400

    updated batch_size to 1200

--- server/search_awards.py
+++ server/search_awards.py
@@ -159,7 +159,7 @@ def update_rates():
 
 if __name__ == "__main__":
     try:
-        search_awards(search_frequency_hours=24, search_batch_size=1000)
+        search_awards(search_frequency_hours=24, search_batch_size=1200)
         update_rates()
     except Exception as e:
         logging.error("Error in main function: %s", str(e))  # Log exceptions
