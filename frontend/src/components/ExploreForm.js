610138ac8f24e02597186b47a090385e6d7bf004
Willie Zhou <williezhou@gmail.com>
2023-07-16 16:16:52 -0700

    added auto-reload back for Explore

--- frontend/src/components/ExploreForm.js
+++ frontend/src/components/ExploreForm.js
@@ -1,8 +1,23 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Select from 'react-select';
+import Select, { components } from 'react-select';
 import Cookies from 'js-cookie';
+
+const Option = (props) => {
+    return (
+        <div>
+            <components.Option {...props}>
+                <input
+                    type="checkbox"
+                    checked={props.isSelected}
+                    onChange={() => null}
+                />{" "}
+                <label>{props.label}</label>
+            </components.Option>
+        </div>
+    );
+};
 
 function ExploreForm({ setStays, isLoading, setIsLoading }) {
     const [awardCategory, setAwardCategory] = useState([{ value: '7', label: '7' }, { value: '8', label: '8' }]);
@@ -10,6 +25,9 @@ function ExploreForm({ setStays, isLoading, setIsLoading }) {
     const [brand, setBrand] = useState([{ value: 'Park Hyatt', label: 'Park Hyatt' }]);
     const [brandOptions, setBrandOptions] = useState([]);
     const [errorMessage, setErrorMessage] = useState(null);
+
+    const prevAwardCategory = useRef(awardCategory);
+    const prevBrand = useRef(brand);
 
     const api_url = process.env.REACT_APP_TEST_API_URL || 'https://hotel-rewards-availability-api.onrender.com'
 
@@ -46,69 +64,95 @@ function ExploreForm({ setStays, isLoading, setIsLoading }) {
         setBrand(sortedOptions);
     };
 
-    const submitForm = async (e) => {
-        e.preventDefault();
+    const handleMenuClose = () => {
+        const submitForm = async () => {
+            // Validation
+            if (awardCategory.length < 1 || brand.length < 1 || awardCategory.length > 3 || brand.length > 3) {
+                setErrorMessage('Please select at least one brand and one category, and no more than three options per category.');
+                return;
+            }
 
-        // Validation
-        if (awardCategory.length < 1 || brand.length < 1 || awardCategory.length > 3 || brand.length > 3) {
-            setErrorMessage('Please select at least one brand and one category, and no more than three options per category.');
-            return;
+            // Check if the values have changed
+            if (JSON.stringify(prevAwardCategory.current) === JSON.stringify(awardCategory) && JSON.stringify(prevBrand.current) === JSON.stringify(brand)) {
+                return;
+            }
+
+            setIsLoading(true);
+            setErrorMessage(null);  // Clear any previous error message
+
+            const awardCategoryArray = Array.isArray(awardCategory) ? awardCategory : [awardCategory];
+            const brandArray = Array.isArray(brand) ? brand : [brand];
+
+            const awardCategoryString = awardCategoryArray.map(category => category.value);
+            const brandString = brandArray.map(brand => brand.value);
+
+            const session_token = Cookies.get('session_token')
+
+            try {
+                const response = await axios.post(api_url + '/api/explore', {
+                    award_category: awardCategoryString,
+                    brand: brandString,
+                    session_token: session_token
+                });
+                setStays(response.data);
+            } catch (error) {
+                console.error(error);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        if (awardCategory.length > 0 && brand.length > 0) {
+            submitForm();
         }
 
-        setIsLoading(true);
-        setErrorMessage(null);  // Clear any previous error message
-
-        const awardCategoryArray = Array.isArray(awardCategory) ? awardCategory : [awardCategory];
-        const brandArray = Array.isArray(brand) ? brand : [brand];
-
-        const awardCategoryString = awardCategoryArray.map(category => category.value);
-        const brandString = brandArray.map(brand => brand.value);
-
-        const session_token = Cookies.get('session_token')
-
-        try {
-            const response = await axios.post(api_url + '/api/explore', {
-                award_category: awardCategoryString,
-                brand: brandString,
-                session_token: session_token
-            });
-            setStays(response.data);
-        } catch (error) {
-            console.error(error);
-        } finally {
-            setIsLoading(false);
-        }
+        // Update the previous values
+        prevAwardCategory.current = awardCategory;
+        prevBrand.current = brand;
     };
 
     return (
-        <form onSubmit={submitForm} className="form-group">
-            <div className="row">
-                <div className="col-md-4">
-                    <label>Award Category:</label>
-                    <Select
-                        options={awardCategoryOptions} 
-                        value={awardCategory}
-                        onChange={handleAwardCategoryChange}
-                        isMulti
-                    />
+        <div className="form-group">
+            {isLoading ? (
+                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
+                    <h4>Loading...</h4>
                 </div>
-                <div className="col-md-4">
-                    <label>Brand:</label>
-                    <Select
-                        options={brandOptions} 
-                        value={brand}
-                        onChange={handleBrandChange}
-                        isMulti
-                    />
-                </div>
-                <div className="col-md-4 d-flex align-items-end">
-                    <button type="submit" className="btn btn-primary w-100" disabled={isLoading}>
-                        {isLoading ? 'Loading...' : 'Explore'}
-                    </button>
-                </div>
-            </div>
-            {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
-        </form>
+            ) : (
+                <>
+                    <div className="row">
+                        <div className="col-md-6">
+                            <label>Award Category:</label>
+                            <Select
+                                options={awardCategoryOptions} 
+                                value={awardCategory}
+                                onChange={handleAwardCategoryChange}
+                                onMenuClose={handleMenuClose}
+                                isMulti
+                                closeMenuOnSelect={false}
+                                hideSelectedOptions={false}
+                                components={{ Option }}
+                                maxMenuHeight={150}  // Adjust this value to your liking
+                            />
+                        </div>
+                        <div className="col-md-6">
+                            <label>Brand:</label>
+                            <Select
+                                options={brandOptions} 
+                                value={brand}
+                                onChange={handleBrandChange}
+                                onMenuClose={handleMenuClose}
+                                isMulti
+                                closeMenuOnSelect={false}
+                                hideSelectedOptions={false}
+                                components={{ Option }}
+                                maxMenuHeight={150}  // Adjust this value to your liking
+                            />
+                        </div>
+                    </div>
+                    {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+                </>
+            )}
+        </div>
     );      
 }
 
