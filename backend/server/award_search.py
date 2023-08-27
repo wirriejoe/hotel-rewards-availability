d07ae894de6efe062cfcab2ed35a0bc6f97b9ebf
Willie Zhou <williezhou@gmail.com>
2023-08-27 06:47:08 -0700

    added proxy ip routing with brightdata

--- backend/server/award_search.py
+++ backend/server/award_search.py
@@ -18,21 +18,31 @@ class AwardSearch:
     def initialize_driver():
         # Load Selenium profile
         load_dotenv(find_dotenv())
-        profile = json.loads(os.getenv('SELENIUM_PROFILE'))
-        # selection = random.choice([1,2,3])
-        # if selection == 1:
-        #     profile = json.loads(os.getenv('SELENIUM_PROFILE'))
-        # elif selection == 2:
-        #     profile = json.loads(os.getenv('SELENIUM_PROFILE_2'))
-        # else:
-        #     profile = profiles.Android()
-        # print(profile)
+        # profile = json.loads(os.getenv('SELENIUM_PROFILE'))
+        selection = random.choice([1,2,3])
+        print(selection)
+        if selection == 1:
+            profile = json.loads(os.getenv('SELENIUM_PROFILE'))
+        elif selection == 2:
+            profile = json.loads(os.getenv('SELENIUM_PROFILE_2'))
+        else:
+            profile = profiles.Android()
+        print(profile)
+
+        # username = 'brd-customer-hl_94decac0-zone-isp'
+        # password = 'fgezgl3cvg66'
+        username = json.loads(os.getenv('BRIGHTDATA_USERNAME'))
+        password = json.loads(os.getenv('BRIHTDATA_PASSWORD'))
+        port = 22225
+        session_id = random.random()
+        super_proxy_url = f"http://{username}-dns-remote-route_err-block-session-{session_id}:{password}@brd.superproxy.io:{port}"
         
         options = ChromeOptions()
         options.add_argument("--headless")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
-        driver = Chrome(profile=profile, options=options,uc_driver=False,injector_options=True)
+        options.add_argument(f"--proxy-server={super_proxy_url}")
+        driver = Chrome(profile=profile, options=options,uc_driver=False,injector_options=True, seleniumwire_options=True)
         driver.execute_cdp_cmd('Runtime.disable',{})
         injector = driver.profiles.injector
 
