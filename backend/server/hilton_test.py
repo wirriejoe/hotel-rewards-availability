40fc6cc358832a63a59096127578d07a91a9c5d7
Willie Zhou <williezhou@gmail.com>
2023-08-28 05:38:46 -0700

    wip hilton crawler

--- backend/server/hilton_test.py
+++ backend/server/hilton_test.py
@@ -1,193 +1,19 @@
 import requests
 import json
 
-url = 'https://www.hilton.com/graphql/customer?appName=dx-res-ui&operationName=hotel_shopAvailOptions_shopPropAvail&originalOpName=getShopAvail&bl=en&ctyhocn=FRAHI'  # Replace this with your GraphQL endpoint URL
+url = 'https://www.hilton.com/graphql/customer?appName=dx-res-ui&operationName=hotel_shopAvailOptions_shopPropAvail&originalOpName=getShopAvail&bl=en&ctyhocn=BJSCAHI'  # Replace this with your GraphQL endpoint URL
 
 headers = {
     'Content-Type': 'application/json',
-    'Authorization': 'Bearer DX.eyJhbGciOiJSU0EtT0FFUCIsImVuYyI6IkEyNTZHQ00iLCJwaWQiOiJ3ZWIiLCJraWQiOiI4UVl1RTZfdHBValdjUmVnem1RZFlBaF9RYlk1ckVqUlpPTThwWmNKeU5BIn0.fTfFgf5o1onJG3fLdILtJ67aD9q1HlAg-9c9_AneFLybRW50wmsNJsQnCDB0Qi-L7W1BsYtXm01kgi89V6wVO2LqhNMY_L2HPMyznWkD8eYvl9gvMflOt35EGhHelRsiu0fD9spVy_ABDqJS41ZfWGQPXq68Yu-tVQk6Po3swfAkfzTakR1cUWjTQszKwAfWbFD-0IKnz7k_O0rHNBBHil4e6oKHsx9e__V1o9qNCxL5JR9NkXE764IBD_A62V5P6I6DyO29XNQB7oOQuLZGtoiiJ772W2FQZw_8-El_4EONT09NaQx6faQsgs4qVDnIXevjCL-i4FpSi0D7cB4-wQ.CflbNsj2TCl0uXB4.j04B7oosy-Va_0JzV43T1vAkpNVfzk_QDJtyc2vb6cdTHNANlXKJ2U32fJb-fMxO6y2cqnqygHIAZXpgAna8gN-JdaA5ekreMRtQSVQgcznDtgVkM57R9DQCkPUF1PIsRTu-xT86zU0AHoHi4xgNnGXwEipKqB4ZIlveUzSQiNv5h-BUyByxjI4HUwcXAdYwNdWG5_MqYjx25eHrh7gJ__QeotVoFWWPov1JiIFUcxao5qLNGRVV6q3aT7IshmHJxOMxtwXIbL5P4mhffaAYLuXLNpyodAL-UnTaPhflLGQ29iEEJXqwgnZORFzvWeW8e6c1l8j-4w712jjFWQ0xvFzB87jtNCqltypKOUwsUbovdAx-tz7xs9KXuFxg9IJf-EMvJLzPNOPHPSKu5ROQrCiFOouixwIhBhUjgTtmIrxg3AIkhqtbjEu8L3on072e_qeGICyJJYtiDFjPlhEDbZYpivHu01d8PvDW67AsRKkhckSl_wMxg8CPQLWj2FBszTEDkwSpwtsB41oXTuAJoCmMZ_viHFRqhPYORMwXebOtPuMRo3IZjlL1eEfkXygTpj67-I499nLIrATJw8450pP-y4Xrr3djUEwrcVEV2XTqXhUea5M5Aic5Hz1U5R95-FcTKutTQeBMOJeE4_UxtLWVHghqN81jL5bH6FyQQiJBhAnx5tE.9aTOvTURpuNrb00aVHj__w'
+    'Authorization': 'Bearer DX.eyJhbGciOiJSU0EtT0FFUCIsImVuYyI6IkEyNTZHQ00iLCJwaWQiOiJ3ZWIiLCJraWQiOiI4UVl1RTZfdHBValdjUmVnem1RZFlBaF9RYlk1ckVqUlpPTThwWmNKeU5BIn0.Mj0NZUxtoEyAhjR_wH6ZDC7Mn4ZlcnkEF0JIg850GOfG8n-xBrTS0s2mHWjOxD-irJuQEMYlY2l52Q1cC2ikqTqM-WS9kPz5ZC68GVqQ4Ub-TkGi8D_XxGMCDWkveCupHV3a2c-WonEA3AyVwfYKhG2EZNIQIdrnAi8Xr7Mg9mPxyogTF3m8M5Wwnagu69Z3YcNf28gXkjrUkgRf7pdFDnqCzgsKylZsIV4KMwOQ6KA7HL7b1ehJi36Ncx89zIOU0L3_lVgmCbs0mmySmjhQt23acFlCOUSaKZ2eO5N3OYCE1-kyrIhrMh-m-mKZuF3NQC1gufuYDbAQ7-fF46ynyQ.eY___-PJ5EQK-vlN.7-E-qbM-Vo3FFVdmxKy48FTv_Q-bhZ9tWYuhTwuxEC6pXSGVYlrs3Ges8my0OpuIEO7jgnFG3xlNoJ_sTtxbXV69NeEBzthrOkVb8PVjp6SfO-BMuM6WFUAoiXZYK3cwfRNSM74exCon8h1iCHIch20RAdiVaGVUz_HSKypuxRNR4XEBZV_0DQR5iY90Un-xJCs6yt1Z4XoOGdVpXLhT06YWLo63Pny-NstsBmCTOJos87cVOcXU_uqWcM8vj2btNDSUxGt6-Q1WUIOAwIb4Bvsyk4Irc__xdkdQAXGXYvEWcpmtwm2xY_b-Va90MdQ3qcMiZ6ACBImzGEGWlPnjbcz5nk1cBOYk6lP_7PrrvDDDMo2iYqaHPHHy5A-8zJZobuZzUE19b7hhhuLlIMglkwDiNfDv7IvXAoM481E5sMxY8n0DqjVwxafgMCcIOcxJdfAkXHnIKkayOj_Ggwq12tCIGIlseha9kDIWO3xx-x4raHbglxdHdiSMxixTKFEuXVrivht3UFvnLNTGaWU2ojZ7e1qZ44Uc1gW4fuD61auU9h3Nj-jgP59EnuIl2qc2mWh_kuC-ar-8NO71lcqxSFj-KYnIFkuchCYaZHroPT1qcBsudbA8Q-j7c1K-TRyNPn85-FpgCcDEhHHY8ZTmE27eBOVI0Oa8OWZPeUiIv4DLwXOPUHAhZHTMweMLwoJpx88cnJRZ3blSpAkoUJnKJh6JLsYLpkokqtkCDY7RkhHsQ5NbPf5Xv9Hp6qzroULlTUOpJ5fDjXGXfB5WRsqgriCHOB75voz2-N4f73nEPhfwI-Tu6iYUdeUQxdaBSpjcSmXIj6nYPEpPCqTqjwGWBFUehN7um3oVjUm-T6vhzQfCZRVgfiwP90kyLpHiHmJmIyhFAY7b_BKN3b7FtubjfFCndBd9dOJnJpcL7OrlFWoU3Lt6sV_sQRuWs8vOsdFfbgisQWg2kyK-vJuK0kCsCnYkGhq7dEOUbGZ1EmD18g5dkbulYXIcrjYqqgLF4YatJ5zWIjUlXsM66imkz3v-tyEGby8PKnxXsBzDWmNS7h2s9PrjUnw7npaebi0mOgeJs8wk5pTy8vj6giCfB7duwuQuxsC9eMSjlzgNUcmhOALhHnsV3McdXIaZF3pVZ0PyWNo.Q4jFFEZZ0TizkyV0ePLKng'
     # You may also need to include authorization or other headers specific to your API
 }
 
+check_in_date = "2023-08-28"
+check_out_date = "2023-08-29"
+
 query = {
-    "query": """
-        query hotel_shopAvailOptions_shopPropAvail($arrivalDate: String!, $ctyhocn: String!, $departureDate: String!, $language: String!, $guestLocationCountry: String, $numAdults: Int!, $numChildren: Int!, $numRooms: Int!, $displayCurrency: String, $guestId: BigInt, $specialRates: ShopSpecialRateInput, $rateCategoryTokens: [String], $selectedRoomRateCodes: [ShopRoomRateCodeInput!], $ratePlanCodes: [String], $pnd: String, $offerId: BigInt, $cacheId: String!, $knownGuest: Boolean, $modifyingReservation: Boolean, $currentlySelectedRoomTypeCode: String, $currentlySelectedRatePlanCode: String, $childAges: [Int], $adjoiningRoomStay: Boolean, $programAccountId: BigInt) {
-        hotel(ctyhocn: $ctyhocn, language: $language) {
-            ctyhocn
-            shopAvailOptions(input: {offerId: $offerId, pnd: $pnd}) {
-            maxNumChildren
-            altCorporateAccount {
-                corporateId
-                name
-            }
-            contentOffer {
-                name
-            }
-            }
-            shopAvail(
-            cacheId: $cacheId
-            input: {guestLocationCountry: $guestLocationCountry, arrivalDate: $arrivalDate, departureDate: $departureDate, displayCurrency: $displayCurrency, numAdults: $numAdults, numChildren: $numChildren, numRooms: $numRooms, guestId: $guestId, specialRates: $specialRates, rateCategoryTokens: $rateCategoryTokens, selectedRoomRateCodes: $selectedRoomRateCodes, ratePlanCodes: $ratePlanCodes, knownGuest: $knownGuest, modifyingReservation: $modifyingReservation, childAges: $childAges, adjoiningRoomStay: $adjoiningRoomStay, programAccountId: $programAccountId}
-            ) {
-            currentlySelectedRoom: roomTypes(
-                filter: {roomTypeCode: $currentlySelectedRoomTypeCode}
-            ) {
-                adaAccessibleRoom
-                roomTypeCode
-                roomRates(filter: {ratePlanCode: $currentlySelectedRatePlanCode}) {
-                ratePlanCode
-                rateAmount
-                rateAmountFmt(decimal: 0, strategy: trunc)
-                amountAfterTaxFmt(decimal: 0, strategy: trunc)
-                fullAmountAfterTax: amountAfterTaxFmt
-                rateChangeIndicator
-                ratePlan {
-                    ratePlanName
-                    commissionable
-                    confidentialRates
-                    specialRateType
-                    hhonorsMembershipRequired
-                    salesRate
-                    redemptionType
-                }
-                pointDetails {
-                    pointsRateFmt
-                }
-                }
-            }
-            statusCode
-            summary {
-                specialRates {
-                specialRateType
-                roomCount
-                }
-                requestedRates {
-                ratePlanCode
-                ratePlanName
-                roomCount
-                }
-            }
-            notifications {
-                subText
-                subType
-                title
-                text
-            }
-            addOnsAvailable
-            currencyCode
-            roomTypes {
-                roomTypeCode
-                adaAccessibleRoom
-                numBeds
-                roomTypeName
-                roomTypeDesc
-                roomOccupancy
-                premium
-                executive
-                towers
-                suite
-                code: roomTypeCode
-                name: roomTypeName
-                adjoiningRoom
-                thumbnail: carousel(first: 1) {
-                _id
-                altText
-                variants {
-                    size
-                    url
-                }
-                }
-                quickBookRate {
-                cashRatePlan
-                roomTypeCode
-                rateAmount
-                rateChangeIndicator
-                feeTransparencyIndicator
-                cmaTotalPriceIndicator
-                ratePlanCode
-                rateAmountFmt(decimal: 0, strategy: trunc)
-                roomTypeCode
-                amountAfterTaxFmt(decimal: 0, strategy: trunc)
-                fullAmountAfterTax: amountAfterTaxFmt
-                ratePlan {
-                    commissionable
-                    confidentialRates
-                    ratePlanName
-                    specialRateType
-                    hhonorsMembershipRequired
-                    salesRate
-                    redemptionType
-                    serviceChargesAndTaxesIncluded
-                }
-                serviceChargeDetails
-                pointDetails(perNight: true) {
-                    pointsRate
-                    pointsRateFmt
-                }
-                }
-                moreRatesFromRate {
-                rateChangeIndicator
-                feeTransparencyIndicator
-                cmaTotalPriceIndicator
-                roomTypeCode
-                rateAmount
-                rateAmountFmt(decimal: 0, strategy: trunc)
-                amountAfterTaxFmt(decimal: 0, strategy: trunc)
-                fullAmountAfterTax: amountAfterTaxFmt
-                serviceChargeDetails
-                ratePlanCode
-                ratePlan {
-                    confidentialRates
-                    serviceChargesAndTaxesIncluded
-                }
-                }
-                bookNowRate {
-                roomTypeCode
-                rateAmount
-                rateChangeIndicator
-                feeTransparencyIndicator
-                cmaTotalPriceIndicator
-                ratePlanCode
-                rateAmountFmt(decimal: 0, strategy: trunc)
-                amountAfterTaxFmt(decimal: 0, strategy: trunc)
-                fullAmountAfterTax: amountAfterTaxFmt
-                roomTypeCode
-                ratePlan {
-                    commissionable
-                    confidentialRates
-                    ratePlanName
-                    specialRateType
-                    hhonorsMembershipRequired
-                    salesRate
-                    disclaimer {
-                    diamond48
-                    }
-                    serviceChargesAndTaxesIncluded
-                }
-                serviceChargeDetails
-                }
-                redemptionRoomRates(first: 1) {
-                rateChangeIndicator
-                pointDetails(perNight: true) {
-                    pointsRate
-                    pointsRateFmt
-                }
-                sufficientPoints
-                pamEligibleRoomRate {
-                    ratePlan {
-                    ratePlanCode
-                    rateCategoryToken
-                    redemptionType
-                    }
-                    roomTypeCode
-                    sufficientPoints
-                }
-                }
-            }
-            lowestPointsInc
-            }
-        }
-        }
-    """,
+    "query": "query hotel_shopAvailOptions_shopPropAvail($arrivalDate: String!, $ctyhocn: String!, $departureDate: String!, $language: String!, $guestLocationCountry: String, $numAdults: Int!, $numChildren: Int!, $numRooms: Int!, $displayCurrency: String, $guestId: BigInt, $specialRates: ShopSpecialRateInput, $rateCategoryTokens: [String], $selectedRoomRateCodes: [ShopRoomRateCodeInput!], $ratePlanCodes: [String], $pnd: String, $offerId: BigInt, $cacheId: String!, $knownGuest: Boolean, $modifyingReservation: Boolean, $currentlySelectedRoomTypeCode: String, $currentlySelectedRatePlanCode: String, $childAges: [Int], $adjoiningRoomStay: Boolean, $programAccountId: BigInt) {\n  hotel(ctyhocn: $ctyhocn, language: $language) {\n    ctyhocn\n    shopAvailOptions(input: {offerId: $offerId, pnd: $pnd}) {\n      maxNumChildren\n      altCorporateAccount {\n        corporateId\n        name\n      }\n      contentOffer {\n        name\n      }\n    }\n    shopAvail(\n      cacheId: $cacheId\n      input: {guestLocationCountry: $guestLocationCountry, arrivalDate: $arrivalDate, departureDate: $departureDate, displayCurrency: $displayCurrency, numAdults: $numAdults, numChildren: $numChildren, numRooms: $numRooms, guestId: $guestId, specialRates: $specialRates, rateCategoryTokens: $rateCategoryTokens, selectedRoomRateCodes: $selectedRoomRateCodes, ratePlanCodes: $ratePlanCodes, knownGuest: $knownGuest, modifyingReservation: $modifyingReservation, childAges: $childAges, adjoiningRoomStay: $adjoiningRoomStay, programAccountId: $programAccountId}\n    ) {\n      currentlySelectedRoom: roomTypes(\n        filter: {roomTypeCode: $currentlySelectedRoomTypeCode}\n      ) {\n        adaAccessibleRoom\n        roomTypeCode\n        roomRates(filter: {ratePlanCode: $currentlySelectedRatePlanCode}) {\n          ratePlanCode\n          rateAmount\n          rateAmountFmt(decimal: 0, strategy: trunc)\n          amountAfterTaxFmt(decimal: 0, strategy: trunc)\n          fullAmountAfterTax: amountAfterTaxFmt\n          rateChangeIndicator\n          ratePlan {\n            ratePlanName\n            commissionable\n            confidentialRates\n            specialRateType\n            hhonorsMembershipRequired\n            salesRate\n            redemptionType\n          }\n          pointDetails {\n            pointsRateFmt\n          }\n        }\n      }\n      statusCode\n      summary {\n        specialRates {\n          specialRateType\n          roomCount\n        }\n        requestedRates {\n          ratePlanCode\n          ratePlanName\n          roomCount\n        }\n      }\n      notifications {\n        subText\n        subType\n        title\n        text\n      }\n      addOnsAvailable\n      currencyCode\n      roomTypes {\n        roomTypeCode\n        adaAccessibleRoom\n        numBeds\n        roomTypeName\n        roomTypeDesc\n        roomOccupancy\n        premium\n        executive\n        towers\n        suite\n        code: roomTypeCode\n        name: roomTypeName\n        adjoiningRoom\n        thumbnail: carousel(first: 1) {\n          _id\n          altText\n          variants {\n            size\n            url\n          }\n        }\n        quickBookRate {\n          cashRatePlan\n          roomTypeCode\n          rateAmount\n          rateChangeIndicator\n          feeTransparencyIndicator\n          cmaTotalPriceIndicator\n          ratePlanCode\n          rateAmountFmt(decimal: 0, strategy: trunc)\n          roomTypeCode\n          amountAfterTaxFmt(decimal: 0, strategy: trunc)\n          fullAmountAfterTax: amountAfterTaxFmt\n          ratePlan {\n            commissionable\n            confidentialRates\n            ratePlanName\n            specialRateType\n            hhonorsMembershipRequired\n            salesRate\n            redemptionType\n            serviceChargesAndTaxesIncluded\n          }\n          serviceChargeDetails\n          pointDetails(perNight: true) {\n            pointsRate\n            pointsRateFmt\n          }\n        }\n        moreRatesFromRate {\n          rateChangeIndicator\n          feeTransparencyIndicator\n          cmaTotalPriceIndicator\n          roomTypeCode\n          rateAmount\n          rateAmountFmt(decimal: 0, strategy: trunc)\n          amountAfterTaxFmt(decimal: 0, strategy: trunc)\n          fullAmountAfterTax: amountAfterTaxFmt\n          serviceChargeDetails\n          ratePlanCode\n          ratePlan {\n            confidentialRates\n            serviceChargesAndTaxesIncluded\n          }\n        }\n        bookNowRate {\n          roomTypeCode\n          rateAmount\n          rateChangeIndicator\n          feeTransparencyIndicator\n          cmaTotalPriceIndicator\n          ratePlanCode\n          rateAmountFmt(decimal: 0, strategy: trunc)\n          amountAfterTaxFmt(decimal: 0, strategy: trunc)\n          fullAmountAfterTax: amountAfterTaxFmt\n          roomTypeCode\n          ratePlan {\n            commissionable\n            confidentialRates\n            ratePlanName\n            specialRateType\n            hhonorsMembershipRequired\n            salesRate\n            disclaimer {\n              diamond48\n            }\n            serviceChargesAndTaxesIncluded\n          }\n          serviceChargeDetails\n        }\n        redemptionRoomRates(first: 1) {\n          rateChangeIndicator\n          pointDetails(perNight: true) {\n            pointsRate\n            pointsRateFmt\n          }\n          sufficientPoints\n          pamEligibleRoomRate {\n            ratePlan {\n              ratePlanCode\n              rateCategoryToken\n              redemptionType\n            }\n            roomTypeCode\n            sufficientPoints\n          }\n        }\n      }\n      lowestPointsInc\n    }\n  }\n}",
     "operationName": "hotel_shopAvailOptions_shopPropAvail",
     "variables": {
         "arrivalDate": "2023-08-28",
@@ -195,14 +21,36 @@ query = {
         "numAdults": 2,
         "numChildren": 0,
         "numRooms": 1,
-        "ctyhocn": "LAXWAWA",
-        "cacheId": "8ece214e-3fe0-47a0-8bb0-012f56fd25d7",
-        "language": "en"
+        "ctyhocn": "BJSCAHI",
+        "language": "en",
+        "specialRates": {
+            # "aaa": False,
+            # "aarp": False,
+            # "governmentMilitary": False,
+            "hhonors": True,
+            # "pnd": "",
+            # "senior": False,
+            # "teamMember": False,
+            # "owner": False,
+            # "ownerHGV": False,
+            # "familyAndFriends": False,
+            # "travelAgent": False,
+            # "smb": False,
+            # "specialOffer": False,
+        },
+        "cacheId": "d4617320-b176-4c26-934f-7216ac72b7aa"
     }
 }
 
 response = requests.post(url, json=query, headers=headers)
 
+awards = response.json()['data']['hotel']['shopAvail']['roomTypes']
+
+for award in awards:
+    room_type_code = award['roomTypeCode']
+    room_name = award['name']
+    room_category = 'Suite' if award['suite'] else 'Standard'
+    # lowest_points_rate = 
 
 
 if response.status_code == 200:
